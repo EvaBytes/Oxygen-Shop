2947e0211b2ee247b1e565f5e7534388b76ed3e3
Eva Sevillian <evasevillanop@gmail.com>
2024-10-30 11:27:46 +0100

    Button issue fixed

--- JavaScript/hamburger_menu.js
+++ JavaScript/hamburger_menu.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", function() {
     const hamburger = document.getElementById("hamburger");
     const mobileMenu = document.getElementById("mobileMenu");
 
-    // Toggle the 'active' class to show/hide the menu on mobile only
     hamburger.addEventListener("click", () => {
         mobileMenu.classList.toggle("active");
     });
--- JavaScript/return_top.js
+++ JavaScript/return_top.js
@@ -3,7 +3,7 @@ const mybutton = document.getElementById("top-button");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
