1aea3b3f763af7eed590c3f50dc954a28a883be0
Eva Sevillian <evasevillanop@gmail.com>
2024-10-31 15:04:33 +0100

    Form started

--- JavaScript/percentage_scroller.js
+++ JavaScript/percentage_scroller.js
@@ -2,12 +2,9 @@ window.addEventListener("load", () => {
     const progress = document.getElementById("percentageScroller");
 
     window.addEventListener ("scroll", () => {
-        const h = this.document.documentElement;
-
-        const st = h.scrollTop || this.document.body.scrollTop;
-    
-        const sh = h.scrollHeight || this.document.body.scrollHeight;
-    
+        const h = document.documentElement;
+        const st = h.scrollTop || document.body.scrollTop;
+        const sh = h.scrollHeight || document.body.scrollHeight;
         const percent = (st / (sh - h.clientHeight)) * 100;
     
         progress.style.width = percent + "%";
--- JavaScript/validate_form.js
+++ JavaScript/validate_form.js
@@ -1,9 +1,14 @@
-const nameInput = document.getElementById("name");
-if (nameValue.length < 2 || nameValue.length > 100) {
-    nameInput.style.borderColor = "red";
-    isValid = false;
-} else {
-    nameInput.style.borderColor = ""; 
-}
 
+document.getElementById('contactForm').addEventListener("send")
+    
+//¿Qué elementos queremos que sean rellenados?
+    const nameInput = document.getElementById('name');
+    const emailInput = document.getElementById('email');
+    const consentCheckbox = document.getElementById('consentCheckbox');
 
+  
+//Para validar el nombre y el email, le establecemos la longitud que queremos min y máx
+    if (nameInput.value.length < 2 || nameInput.value.length > 100) {
+      nameInput.style.border = '2px solid red';
+      isValid = false;
+    };
